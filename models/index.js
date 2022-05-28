d1082628286b6558dac25eac716870a9807d8752
Mateo <mtaycruz@gmail.com>
2022-05-28 00:21:23 -0400

    latest release

--- models/index.js
+++ models/index.js
@@ -11,7 +11,10 @@ const db = {};
 
 // don't want to pluralize table name
 config.define = {"freezeTableName" : true}
-
+config.dialectOptions = {ssl: {
+  require: true,
+  rejectUnauthorized: false, // very important
+}}
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
